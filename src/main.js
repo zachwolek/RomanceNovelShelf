b2a16f225e003cd55f9c9eebc0b713cbcea2d8d0
Lydia Sims <sims.lydia88@gmail.com>
2024-02-07 18:09:19 -0600

    Create the showRandomCover function

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // Create variables targetting the relevant DOM elements here 👇
+var showRandomButton = document.querySelector(".random-cover-button");
+var coverImage = document.querySelector(".cover-image");
+var coverTitle = document.querySelector(".cover-title")
+var tag1 = document.querySelector(".tagline-1")
+var tag2 = document.querySelector(".tagline-2")
 
 
 // We've provided a few variables below
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = showRandomCover();
 
 // Add your event listeners here 👇
 
@@ -28,3 +33,23 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
   }
   return cover
 }
+
+//for this function we need to get a random cover, a random title, and two random taglines from the arrays on the data.js file to appear on the screen. So there needs to be variables for randomCover, randomTitle, randomTagline1 and randomTagline2. we also need to create a variable for the new cover
+//we need to use the getRandomIndex function and the createCover functions to get the data needed. we also need to access the items on the DOM
+
+//This is running currentCover = showRandomCover();
+function showRandomCover(){
+  var randomCover = covers[getRandomIndex(covers)];
+  var randomTitle = titles[getRandomIndex(titles)];
+  var randomTagline1 = descriptors[getRandomIndex(descriptors)];
+  var randomTagline2 = descriptors[getRandomIndex(descriptors)];
+  var newCover = createCover(randomCover, randomTitle, randomTagline1,randomTagline2);
+  currentCover = newCover;
+  coverImage.src = currentCover.coverImg;
+  coverTitle.innerText = currentCover.title;
+  tag1.innerText = currentCover.tagline1;
+  tag2.innerText = currentCover.tagline2;
+
+  return currentCover;
+}
+
