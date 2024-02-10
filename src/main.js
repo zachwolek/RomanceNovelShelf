ea21a0a271d5a73baf19b321fa813baeb5ae502a
Zach Wolek <zach.wolek@gmail.com>
2024-02-10 15:44:51 -0600

    clean up the JS in readyment for combined merge

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Create variables targetting the relevant DOM elements here 👇
 //sections  
-var controlSection = document.querySelector(".controls"); // I think we can delete this
+
 var mainCoverSection = document.querySelector(".main-cover");
 var formViewSection = document.querySelector(".form-view");
 var savedCoverSection = document.querySelector(".saved-covers-section");
@@ -16,7 +16,17 @@ var homeButton = document.querySelector(".home-button")
 var randomCoverButton = document.querySelector(".random-cover-button")
 var saveCoverButton = document.querySelector(".save-cover-button")
 var viewSavedButton = document.querySelector(".view-saved-button")
-var makeNewButton = document.querySelector(".make-new-button")
+var makeNewButton = document.querySelector(".make-new-button");
+var createNewCoverButton = document.querySelector(".create-new-book-button")
+//form Sections
+
+
+
+
+
+
+
+
 
 
 // We've provided a few variables below
@@ -25,25 +35,57 @@ var savedCovers = [
 ];
 var currentCover ;
 
+
+
+
+
+
+
+
+
 // Add your event listeners here 👇
 addEventListener('load', showRandomCover);
-
-//We need to add Home button
 randomCoverButton.addEventListener('click', showRandomCover);
-makeNewButton.addEventListener('click', clickMakeNewButton)
-viewSavedButton.addEventListener('click', clickViewSavedButton)
-homeButton.addEventListener('click', clickHomeButton)
+makeNewButton.addEventListener('click', clickMakeNewButton);
+viewSavedButton.addEventListener('click', clickViewSavedButton);
+homeButton.addEventListener('click', clickHomeButton);
+createNewCoverButton.addEventListener('click', createCustomCover);
+saveCoverButton.addEventListerner('click', clickSaveButton);
 
 
 // Create your event handlers and other functions here 👇
+
+//for this function we need to get a random cover, a random title, and two random taglines from the arrays on the data.js file to appear on the screen. So there needs to be variables for randomCover, randomTitle, randomTagline1 and randomTagline2. we also need to create a variable for the new cover
+//we need to use the getRandomIndex function and the createCover functions to get the data needed. we also need to access the items on the DOM
+
+function updateMainCover() {
+  coverImage.src = currentCover.coverImg;
+  coverTitle.innerText = currentCover.title;
+  tag1.innerText = currentCover.tagline1;
+  tag2.innerText = currentCover.tagline2;
+}
+
+function showRandomCover(){
+  var randomCover = covers[getRandomIndex(covers)];
+  var randomTitle = titles[getRandomIndex(titles)];
+  var randomTagline1 = descriptors[getRandomIndex(descriptors)];
+  var randomTagline2 = descriptors[getRandomIndex(descriptors)];
+  var newCover = createCover(randomCover, randomTitle, randomTagline1,randomTagline2);
+  currentCover = newCover;
+  updateMainCover();
+  
+  return currentCover;
+}
+
 //This initiates when "Make Your Own Cover" button is clicked
 function clickMakeNewButton(){
   randomCoverButton.classList.add('hidden');
   saveCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
   formViewSection.classList.remove('hidden');
-//below ensures the rest are hidden
-  viewHome.classList.add('hidden');
+  //below ensures the rest are hidden
+  mainCoverSection.classList.add('hidden');
+  viewHome.classList.add('hidden')
   viewSavedSection.classList.add('hidden');
   savedCoverSection.classList.add('hidden');
 }
@@ -55,7 +97,8 @@ function clickViewSavedButton(){
   homeButton.classList.remove('hidden');
   savedCoverSection.classList.remove('hidden');
   //below ensures the rest are hidden
-  viewHome.classList.add('hidden');
+  mainCoverSection.classList.add('hidden');
+  viewHome.classList.add('hidden')
   formViewSection.classList.add('hidden');
   viewSavedSection.classList.add('hidden');
 }
@@ -64,15 +107,38 @@ function clickHomeButton(){
   randomCoverButton.classList.remove('hidden');
   saveCoverButton.classList.remove('hidden');
   homeButton.classList.add('hidden');
-  viewHome.classList.remove('hidden');
+  mainCoverSection.classList.remove('hidden');
+  viewHome.classList.remove('hidden')
   //below ensures the rest are hidden
   formViewSection.classList.add('hidden');
   savedCoverSection.classList.add('hidden');
   viewSavedSection.classList.add('hidden');
 }
+//<--------------ITERATION 3---------------->
+var savedCovers = [];
+function clickSaveButton (){
+  //Goal: When a user clicks the “View Saved Covers” button, we should see the saved covers section
+  ///This changes the section to SavedCover Section
+  savedCoverSection.classList.remove('hidden');
+  //below ensures the rest are hidden
+  formViewSection.classList.add('hidden');
+  viewHome.classList.add('hidden')
+  viewSavedSection.classList.add('hidden');
+
+  //Goal: When a user clicks the “Save Cover” button, the current cover will be added to the savedCovers array
+//this is the variable being returned from createCustomCover(){
+
+  //Goal: Save a cover exactly nce:
+//if the array savedCovers doesn't include current cover, push saved cover
+if (!savedCovers.includes(currentCover)){
+      savedCovers.push(currentCover)
+ }
+
+ //Goal: All the covers in the savedCovers array should be displayed in the saved covers section
 
 
-
+};
+//--------------END ITERATION 3---------------->
 
 // We've provided two functions to get you started
 function getRandomIndex(array) {
@@ -90,21 +156,44 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
   return cover
 }
 
-//for this function we need to get a random cover, a random title, and two random taglines from the arrays on the data.js file to appear on the screen. So there needs to be variables for randomCover, randomTitle, randomTagline1 and randomTagline2. we also need to create a variable for the new cover
-//we need to use the getRandomIndex function and the createCover functions to get the data needed. we also need to access the items on the DOM
 
-//This is running currentCover = showRandomCover();
-function showRandomCover(){
-  var randomCover = covers[getRandomIndex(covers)];
-  var randomTitle = titles[getRandomIndex(titles)];
-  var randomTagline1 = descriptors[getRandomIndex(descriptors)];
-  var randomTagline2 = descriptors[getRandomIndex(descriptors)];
-  var newCover = createCover(randomCover, randomTitle, randomTagline1,randomTagline2);
-  currentCover = newCover;
-  coverImage.src = currentCover.coverImg;
-  coverTitle.innerText = currentCover.title;
-  tag1.innerText = currentCover.tagline1;
-  tag2.innerText = currentCover.tagline2;
+// for this function i need to create a cover that is custom. the parts of the cover come from the input fields on the form view of the webpage. 
+// i need to make DOM variable for the input fields and the button  to create the new cover 
+// create a variable for custom cover that envokes createCover
+//current cover is assigned to the customCover
+//replicate code from show random cover
 
-  return currentCover;
+function createCustomCover(event){
+  var userCover = document.getElementById("cover").value
+  var userTitle = document.getElementById("title").value
+  var userDescriptor1 = document.getElementById("descriptor1").value
+  var userDesciptor2 = document.getElementById("descriptor2").value  
+  //create custom cover variale
+  var customCover = createCover(userCover,userTitle, userDescriptor1, userDesciptor2)
+
+  //assign current to custom
+  currentCover = customCover
+  // replication from previous code that gets the switches data on the DOM
+  updateMainCover();
+
+
+  //to view the current poster 
+  clickHomeButton();
+  
+  //consoles to see what is happening in the code 
+  // console.log("custom",customCover)
+  // console.log("current",currentCover)
+  
+  // console.log(userCover)
+  // console.log(userTitle)
+  // console.log(userDescriptor1)
+  // console.log(userDesciptor2)
+
+  // to prevent random poster
+  //Is something weird happening when you click the button? Try googling event.preventDefault()!
+  //We didn't enter a parameter, so event doesn't get passed, and this below doesn't do anything
+  event.preventDefault()
+
 }
+  console.log(currentCover)
+  //Coding Notes: Fixed the issue of the cover object being displayed in the form section. values from the input boxed are being ran and the consoles prove that the values are being recorded. It is just not pushing into the DOM object. 
